05c4d80f8a46344d128fe899be11d816a62d1968
Thejellyfish1024 <alamin102410@gmail.com>
2023-11-07 22:23:59 +0600

    added functionality for updating a blog

--- src/pages/UpdateDetails/UpdateDetails.jsx
+++ src/pages/UpdateDetails/UpdateDetails.jsx
@@ -1,14 +1,15 @@
 import { useParams } from "react-router-dom";
 import useBlogs from "../../hooks/useBlogs";
+import Swal from "sweetalert2";
 
 
 const UpdateDetails = () => {
     const { id } = useParams()
-    console.log(id);
+    // console.log(id);
 
     const {data, isLoading} = useBlogs(`blogs/${id}`) 
      
-    console.log(data);
+    // console.log(data);
     if(isLoading){
         return <div>
             <h2 className="text-3xl font-bold text-center my-10">Loading...</h2>
@@ -16,11 +17,47 @@ const UpdateDetails = () => {
     }
     const {title, img , email, short_description, long_description} = data;
 
+    const handleUpdate = e => {
+        e.preventDefault();
+        const form = e.target;
+        const title = form.title.value;
+        const img = form.img.value;
+        const category = form.category.value.toLowerCase();
+        const short_description = form.short_description.value;
+        const long_description = form.long_description.value;
+
+        
+        const updatedBlog = {
+            title, img, category, short_description, long_description
+        }
+        // console.log(updatedBlog);
+        fetch(`http://localhost:5000/blogs/${id}`, {
+            method:'PUT',
+            headers:{
+                'content-type' : 'application/json'
+            },
+            body:JSON.stringify(updatedBlog)
+
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log(data);
+            if(data?.modifiedCount > 0){
+                Swal.fire({
+                    title: 'Updated Successfully!',
+                    icon: 'success',
+                    confirmButtonText: 'Close'
+                  })
+            }
+        })
+
+    }
+
     return (
         <div className="p-3 my-10">
             <div className="max-w-7xl mx-auto bg-base-300 p-4 rounded-xl">
                 <h2 className="text-3xl font-bold text-center my-8 text-purple-500">Update Product</h2>
-                <form className="p-5">
+                <form onSubmit={handleUpdate} className="p-5">
                     <div className="md:flex gap-12">
                         <div className="md:w-1/2">
                             <p className="text-xl font-semibold my-3">Blog Title</p>
@@ -67,7 +104,7 @@ const UpdateDetails = () => {
 
 
                     <button className="btn btn-success text-xl font-bold mt-5">
-                        <input type="submit" value="Submit" />
+                        <input type="submit" value="Update" />
                     </button>
 
                 </form>
